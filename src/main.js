be1da4e57ef2579c10444474e4cdcfd28aacea30
yongqing.cai <459987870>
2021-09-24 14:47:51 +0800

    back

--- src/main.js
+++ src/main.js
@@ -7,13 +7,7 @@ import "element-ui/lib/theme-chalk/index.css";
 
 import "./mock";
 import request from "@/api/request";
-import VueMindmap from "vue-mindmap";
-import "vue-mindmap/dist/vue-mindmap.css";
-import SuperFlow from "vue-super-flow";
-import "vue-super-flow/lib/index.css";
 
-Vue.use(SuperFlow);
-Vue.use(VueMindmap);
 Vue.prototype.request = request;
 
 Vue.config.productionTip = false;
